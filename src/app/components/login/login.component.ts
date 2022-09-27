eabd90546dcccb2bb0d9140b15f4e42f7d66fa82
sahithi <aendapallysahithireddy@gmail.com>
2022-09-27 09:23:46 +0530

    login html

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  hide = true;
 
-  constructor() { }
+  loginForm!: FormGroup;
+  username = new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]);
+  password = new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]);
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: this.username,
+      password: this.password
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.loginForm.value);
+
   }
 
 }
